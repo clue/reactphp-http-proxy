1889b0e12b456fda1083dfcfae379831c5bf0451
Simon Frings <simon@clue.engineering>
2020-10-14 13:12:10 +0200

    Update PHPUnit configuration schema for PHPUnit 9.3

--- tests/AbstractTestCase.php
+++ tests/AbstractTestCase.php
@@ -52,19 +52,29 @@ abstract class AbstractTestCase extends \PHPUnit\Framework\TestCase
      */
     protected function createCallableMock()
     {
-        return $this->getMockBuilder('stdClass')->setMethods(array('__invoke'))->getMock();
+        if (method_exists('PHPUnit\Framework\MockObject\MockBuilder', 'addMethods')) {
+            // PHPUnit 8.5+
+            return $this->getMockBuilder('stdClass')->addMethods(array('__invoke'))->getMock();
+        } else {
+            // legacy PHPUnit 4 - PHPUnit 8.4
+            return $this->getMockBuilder('stdClass')->setMethods(array('__invoke'))->getMock();
+        }
     }
 
-    public function setExpectedException($exception, $message = '', $code = 0)
+    public function setExpectedException($exception, $exceptionMessage = '', $exceptionCode = null)
     {
         if (method_exists($this, 'expectException')) {
+            // PHPUnit 5.2+
             $this->expectException($exception);
-            if ($message !== '') {
-                $this->expectExceptionMessage($message);
+            if ($exceptionMessage !== '') {
+                $this->expectExceptionMessage($exceptionMessage);
             }
-            $this->expectExceptionCode($code);
+            if ($exceptionCode !== null) {
+                $this->expectExceptionCode($exceptionCode);
+            }
         } else {
-            parent::setExpectedException($exception, $message, $code);
+            // legacy PHPUnit 4 - PHPUnit 5.1
+            parent::setExpectedException($exception, $exceptionMessage, $exceptionCode);
         }
     }
 
